edb53cdbd24b46a84b86d36a08beaa61488c8620
Afonso Alcantara <alcantara.afonso@gmail.com>
2024-10-03 18:12:31 -0300

    Customer Use case unit test

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,51 @@
+import CreateCustomerUseCase from "./create.customer.usecase";
+
+const input = {
+    name: "John",
+    address: {
+        street: "Street",
+        number: 123,
+        zip: "Zip",
+        city: "City",
+    },
+};
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit Test create customer use case", () => {
+    it("should create a customer", async () => {
+        const customerRepository = MockRepository();
+        const usecase = new CreateCustomerUseCase(customerRepository);
+
+        const output = await usecase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                number: input.address.number,
+                zip: input.address.zip,
+                city: input.address.city,
+            },
+        });
+
+    });
+
+    it ("should throw an error when name is empty", async () => { 
+        const customerRepository = MockRepository();
+        const usecase = new CreateCustomerUseCase(customerRepository);
+
+        input.name = "";
+        
+
+        await expect(usecase.execute(input)).rejects.toThrow("Nome inválido");
+    });
+});
